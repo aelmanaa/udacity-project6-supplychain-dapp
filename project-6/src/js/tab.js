b2fde32d42fe3e98d61a9dc34bbd8c88533d7ddd
Myamoto <aelmanaa@gmail.com>
2021-05-25 22:46:56 +0200

    DAPP frontend

---
+++ project-6/src/js/tab.js
@@ -0,0 +1,14 @@
+import '../css/tab.css'
+
+window.openPage =  (pageName) => {
+    // Hide all elements with class="tabcontent" by default */
+    var i, tabcontent;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Show the specific tab content
+    document.getElementById(pageName).style.display = "block";
+
+}
